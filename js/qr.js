78be3d696badd8b34598b7fcce44398824d8d60c
Jordanium01 <jo.urzua@duocuc.cl>
2022-12-03 16:26:19 -0300

    para que me tome el git.io

--- js/qr.js
+++ js/qr.js
@@ -1,2 +1,6 @@
 const contenedorQR = document.getElementById('contenedorQR');
 const QR = new QRCode(contenedorQR, 'https://github.com/MarceloContrerasGonzalez/PaginaUberIonicDesign/raw/master/TeLLevoAPP.apk');
+
+
+
+console.log(navigator.platform)
