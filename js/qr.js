0e62bfa7ce400267ec940432711088dafd1ce80c
Jordanium01 <jo.urzua@duocuc.cl>
2022-12-08 13:17:45 -0300

    a1

--- js/qr.js
+++ js/qr.js
@@ -1,20 +1,31 @@
 //Crear el qr
+//Qr del header
 const contenedorQR = document.getElementById('contenedorQR');
 const QR = new QRCode(contenedorQR, 'https://github.com/MarceloContrerasGonzalez/PaginaUberIonicDesign/raw/master/TeLLevoAPP.apk');
+//Qr del footer
+const contenedorQR2 = document.getElementById('contenedorQR2');
+const QR2 = new QRCode(contenedorQR2, 'https://github.com/MarceloContrerasGonzalez/PaginaUberIonicDesign/raw/master/TeLLevoAPP.apk');
 
+//Boton de arriba
 const contenedorBoton = document.getElementById('cont_boton');
+//Boton del footer
+const contenedorBoton2 = document.getElementById('cont_boton2');
 
 
 let bolMobile = getMobileOS();
 
 if (bolMobile == true){
     let text = document.getElementById('textQR')
+    let text2 = document.getElementById('textQR2')
     //Si estas en movil, ocultar el qr
     text.style.display = "none"
+    text2.style.display = "none"
     contenedorQR.style.display = "none";
+    contenedorQR2.style.display = "none";
 } else {
     //Si estas en pc, ocultar el boton de descarga
     contenedorBoton.style.display = "none";
+    contenedorBoton2.style.display = "none";
 }
 //splat.textContent= plat.toString();
 
@@ -44,4 +55,3 @@ function getMobileOS() {
     //return "pc";
     return false;
 }
-
