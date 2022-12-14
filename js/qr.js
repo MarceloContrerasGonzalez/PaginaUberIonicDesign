3dd0de3f8c880668f182b379daca38136b346e3c
Jordanium01 <jo.urzua@duocuc.cl>
2022-12-14 20:37:18 -0300

    v. final final

--- js/qr.js
+++ js/qr.js
@@ -33,25 +33,10 @@ if (bolMobile == true){
 function getMobileOS() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    if ( /*(/windows phone/i.test(userAgent)) ||*/ (/android/i.test(userAgent)) || (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)){
+    if ((/android/i.test(userAgent)) || (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)){
         console.log(userAgent)
         return true;
     }
-    // Windows Phone must come first because its UA also contains "Android"
-   /* if (/windows phone/i.test(userAgent)) {
-        return "Windows Phone";
-    }
-
-    if (/android/i.test(userAgent)) {
-        return "Android";
-    }
-
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "iOS";
-    }
-    */
-
     //return "pc";
     return false;
 }
--- js/styleJS.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function (oEvent) {
-    var mydivpos = document.getElementById("div1").offsetTop;
-    var scrollPos = document.getElementsByTagName("section")[0].scrollTop;
-  
-    if(scrollPos >= mydivpos) document.getElementById("noshow").style.display = "none"
-    /*else
-      document.getElementById("noshow").style.display = "none"*/
-  };
